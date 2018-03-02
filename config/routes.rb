f2b28f624d67cfdfd7be26b6892c97cc2654944d
Tasmin Steer <tasminsteer@hotmail.com>
2018-03-02 14:47:40 +0000

    Improve routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  resources :coaches
-  resources :players
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :comments
-  get 'pages/about'
-  get 'pages/resources'
 
   root to: redirect('/articles')
+
   resources :articles, only: [:show, :index]
   resources :players, only: [:index]
   resources :coaches, only: [:index]
   resources :comments, only: [:index, :new, :create]
 
+  scope :pages do
+    get :resources, to: 'pages#resources', as: :resources
+    get :about, to: 'pages#about', as: :about
+  end
 end
