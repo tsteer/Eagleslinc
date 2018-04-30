5ce9291a7fbcbd0d6abb0a1fde8101f1c46dc1a1
tsteer <tasminsteer@hotmail.com>
2018-04-30 23:28:47 +0100

    thirs try

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
   tinyMCE.init({
     mode: 'specific_textareas',
     editor_deselector : "mceNoEditor",
+    extended_valid_elements: "iframe[src|width|height|name|align], embed[width|height|name|flashvars|src|bgcolor|align|play|loop|quality|allowscriptaccess|type|pluginspage]",
     plugins: "media",
     theme: "modern", 
     theme_advanced_buttons1: "code", 
