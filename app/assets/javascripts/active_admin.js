1bd1358da591d9d754252caa8bb7e424eeeb7b5e
tsteer <tasminsteer@hotmail.com>
2018-04-30 22:27:52 +0100

    try to add embed to tinymce

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -3,8 +3,11 @@
 
 $(document).ready(function() {
   tinyMCE.init({
-     mode: 'specific_textareas',
-     theme: 'modern',
-     editor_deselector : "mceNoEditor"
+    mode: 'specific_textareas',
+    editor_deselector : "mceNoEditor"
+    plugins: "media",
+    theme: "advanced", 
+    theme_advanced_buttons1: "code", 
+    media_strict: false 
    });
 });
